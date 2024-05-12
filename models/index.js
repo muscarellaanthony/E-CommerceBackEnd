5092d727a920995899fd920a00c67114d8a5cc1f
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-05-12 16:01:19 -0400

    added product routes

--- models/index.js
+++ models/index.js
@@ -13,9 +13,19 @@ Category.hasMany(Product,{
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag});
+Product.belongsToMany(Tag, {
+  through: { 
+    model:ProductTag
+  },
+
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag})
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+})
+
 module.exports = {
   Product,
   Category,
